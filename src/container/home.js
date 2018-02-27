f1040425cd4d40675ed2a3455b26ada6d115ec3e
José Támara <jochitoel1@hotmail.com>
2018-02-26 19:29:07 -0500

    Add files via upload

---
+++ src/container/home.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+class Home extends Component {
+	render(){
+			return <div>¡¡¡Hi Támara!!!</div>
+		}
+		
+}
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {render} from 'react-dom';
+import Home from "./container/home";
+
+const app = document.getElementById("app");
+render(<Home />, app);
